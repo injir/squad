0d519bf16804867190792df8adb002cff9b55153
Injir <Tworevolver@gmail.com>
2016-07-01 18:47:31 +0300

    UE4 project init

---
+++ Source/squadManager/squadManagerGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "squadManagerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUADMANAGER_API AsquadManagerGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
