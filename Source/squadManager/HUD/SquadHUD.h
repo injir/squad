938990c75f0d0a065be05c77ebadce17a25b17c3
Risum <pasha.klepachev@gmail.com>
2016-07-05 20:46:41 +0300

    Base HUD class and UMG dependencies

---
+++ Source/squadManager/HUD/SquadHUD.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "SquadHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUADMANAGER_API ASquadHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
