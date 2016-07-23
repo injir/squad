8a4fd79b9af6b479e45d8d6d72d77f68792203ac
Injir <Tworevolver@gmail.com>
2016-07-23 14:41:47 +0300

    Squad interface

---
+++ Source/squadManager/SquadGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SquadGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUADMANAGER_API ASquadGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/squadManager/SquadPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "SquadPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUADMANAGER_API ASquadPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
